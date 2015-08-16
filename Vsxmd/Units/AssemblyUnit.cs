eddcf5110cc1d5d44e8e2bb529c87887d50abe33
Junle Li <lijunle@gmail.com>
2015-08-16 14:03:55 +0800

    Add convert-unit pattern to generate markdown.
    - Only assembly unit is finished.
    - Generated md file is almost cleaned up.

---
+++ Vsxmd/Units/AssemblyUnit.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyUnit.cs" company="Junle Li">
+//     Copyright (c) Junle Li. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Vsxmd.Units
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// Assembly unit.
+    /// </summary>
+    internal class AssemblyUnit : IUnit
+    {
+        private readonly XElement element;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyUnit"/> class.
+        /// </summary>
+        /// <param name="element">The assembly XML element.</param>
+        /// <exception cref="ArgumentException">Throw if XML element name is not <c>assembly</c>.</exception>
+        public AssemblyUnit(XElement element)
+        {
+            if (element.Name != "assembly")
+            {
+                throw new ArgumentException(nameof(element));
+            }
+
+            this.element = element;
+        }
+
+        private string AssemblyName => this.element.Element("name").Value;
+
+        /// <inheritdoc />
+        public IEnumerable<string> ToMarkdown() =>
+            new[]
+            {
+                $"# {this.AssemblyName}"
+            };
+    }
+}
---
+++ Vsxmd/Units/IUnit.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IUnit.cs" company="Junle Li">
+//     Copyright (c) Junle Li. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Vsxmd.Units
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// <see cref="IUnit"/> is wrapper to handle XML elements.
+    /// </summary>
+    internal interface IUnit
+    {
+        /// <summary>
+        /// Represent the XML element content as Markdown syntax.
+        /// </summary>
+        /// <returns>The generated Markdown content.</returns>
+        IEnumerable<string> ToMarkdown();
+    }
+}
