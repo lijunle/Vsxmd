2946ae58437005f32749001d441c1c6276d69d3e
Junle Li <lijunle@gmail.com>
2015-08-16 22:47:49 +0800

    Add BaseUnit and extract common code to it.

--- Vsxmd/Units/AssemblyUnit.cs
+++ Vsxmd/Units/AssemblyUnit.cs
@@ -13,29 +13,22 @@ namespace Vsxmd.Units
     /// <summary>
     /// Assembly unit.
     /// </summary>
-    internal class AssemblyUnit : IUnit
+    internal class AssemblyUnit : BaseUnit
     {
-        private readonly XElement element;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="AssemblyUnit"/> class.
         /// </summary>
         /// <param name="element">The assembly XML element.</param>
         /// <exception cref="ArgumentException">Throw if XML element name is not <c>assembly</c>.</exception>
         public AssemblyUnit(XElement element)
+            : base(element, "assembly")
         {
-            if (element.Name != "assembly")
-            {
-                throw new ArgumentException(nameof(element));
-            }
-
-            this.element = element;
         }
 
-        private string AssemblyName => this.element.Element("name").Value;
+        private string AssemblyName => this.Element.Element("name").Value;
 
         /// <inheritdoc />
-        public IEnumerable<string> ToMarkdown() =>
+        public override IEnumerable<string> ToMarkdown() =>
             new[]
             {
                 $"# {this.AssemblyName}"
