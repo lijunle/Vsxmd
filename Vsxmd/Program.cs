5de51f27e74c8c5b081f174751cc3209fc15e79f
Junle Li <lijunle@gmail.com>
2019-01-01 03:12:10 -0800

    Add empty .Net Core 2.2 project.

---
+++ Vsxmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vsxmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
