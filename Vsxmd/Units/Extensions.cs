4728397d83e3aa6b80ab3b5dbf2e506c85365d56
Junle Li <lijunle@gmail.com>
2019-01-04 12:00:05 -0800

    Remove one unused using.

--- Vsxmd/Units/Extensions.cs
+++ Vsxmd/Units/Extensions.cs
@@ -9,7 +9,6 @@ namespace Vsxmd.Units
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text.RegularExpressions;
     using System.Xml.Linq;
 
     /// <summary>
